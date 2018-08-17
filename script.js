3b5d9d3293286938daa34ed84e068eedf6a07e03
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2018-08-17 00:14:50 +0000

    lesson4

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 $(document).ready(function(){
    alert("jQuery loaded") 
 });
+// Explaining when you would know that jQuery is involved in a website as well as specific jQuery Statements ".css , .animate" 
