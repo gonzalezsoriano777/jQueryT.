8c2d131faee02fd12a7688a533fb5aea36ef8193
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2018-08-17 01:08:52 +0000

    lesson2

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-// It was a brief introduction of whats the difference between jQuery when used in Javascript rather then just using Javascript itself
-=======
-// Overall, What is jQuery and Why it's used..
->>>>>>> lesson2
-=======
-// Overall, What is jQuery and Why it's used..
->>>>>>> lesson2
+// Introduction to jQuery ...
