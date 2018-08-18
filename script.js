5e09887299204d5e8ac0d204ba396e2719e66619
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2018-08-18 14:10:38 +0000

    lesson5

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
-$(document).ready(function(){
-   alert("jQuery loaded") 
-});
-
-/*
-$("#lead-banner");
-*/
 
 
 
-/*Element Selectors*/
+
+
+
+
+
+
+
+
+
+
+
+
+
 $("h3").css({border:"3px solid blue"});
 
-/*class Selectors*/
+
 $(".wrapper").css({border: "3px solid red"});
 
-/* Id Selectors*/
 $("#clients").css({border:"3px solid yellow"}); 
