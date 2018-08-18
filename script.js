6cd469bb15e075c920c25ac6c086d5503ef8a524
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2018-08-18 15:22:29 +0000

    lesson5 -comment

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-<<<<<<< HEAD
+/* Lesson3 
 $(document).ready(function(){
    alert("jQuery loaded") 
 });
+*/
+
 // Explaining when you would know that jQuery is involved in a website as well as specific jQuery Statements ".css , .animate" 
-=======
+
+/* Lesson 5 
 $("h3").css({border:"3px solid blue"});
 
 $(".wrapper").css({border: "3px solid red"});
 
 $("#clients").css({border: "3px solid yellow"});
->>>>>>> lesson5
+*/
