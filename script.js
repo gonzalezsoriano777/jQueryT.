0dc4378c51bbf85d0c2cc17e9892d0065d1e4a95
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2018-08-27 23:41:42 +0000

    lesson19

--- script.js
+++ script.js
@@ -278,4 +278,27 @@ $(document).ready(function(){
     });
     */
 
+// Tutorial 19 - Animations in jQuery
+$(document).ready(function() {
+    
+function complete() {
+        alert("Animation complete");
+    }
+    $("section > h2").on("click", function() {
+     
+     $(this).animate({"width" : "500px", "height" : "100px"}, 
+    1000, "linear", complete);   
+    
+   
+   
+        
+    });
+});
 
+
+
+/*
+
+
+
+*/
